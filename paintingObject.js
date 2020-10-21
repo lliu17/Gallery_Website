364bcb6cb129b17a2b019a632d18420b09015731
Elizabeth Hom <hom.elizabeth@yahoo.com>
2020-10-21 00:58:42 -0400

    reorganized

--- paintingObject.js
+++ paintingObject.js
@@ -18,7 +18,7 @@ var gallery = [
     Painting("3 Birches", MLE, mi, "2'", "3'", 200, "images/MLE_3_birches.jpg"), 
     Painting("8 Birches", MLE, mi, "2'", "3'", 500, "images/MLE_8_birches.jpg"),
     Painting("M-22 Montage", MLE, mi, "2'", "3'", 350, "images/MLE_M_22_montage.jpg"),
-    Painting("Birches and Butterflies", MLE, mi, "2'", "3'", 350, "images/MLE_M_22_montage.jpg");
+    Painting("Birches and Butterflies", MLE, mi, "2'", "3'", 350, "images/MLE_M_22_montage.jpg"),
     Painting("Blue Water, Gold Sand", MLE, mi, "1.5'", "2'", 350, "images/MLE_blue_water_gold_sand.jpg")
     ]
 
